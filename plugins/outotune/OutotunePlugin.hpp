ef5ffbee0d44aa611d07294d4bb616803ad9f918
Richard Hladík <rihl@uralyx.cz>
2020-02-04 22:06:02 +0100

    Pitch detection on a bigger buffer, smooth UI

--- plugins/outotune/OutotunePlugin.hpp
+++ plugins/outotune/OutotunePlugin.hpp
@@ -6,11 +6,12 @@
 
 class OutotunePlugin : public DISTRHO::Plugin {
 public:
-	OutotunePlugin() : Plugin(2, 0, 0), silence_threshold(-70) {
+	OutotunePlugin() : Plugin(2, 0, 0) {
 		size_t frames = getBufferSize();
-		aubio_in = new_fvec(frames);
+		internalFrames = 2 * frames;
+		aubio_in = new_fvec(internalFrames);
 		aubio_out = new_fvec(1);
-		pitch = new_aubio_pitch("default", frames, frames, getSampleRate());
+		pitch = new_aubio_pitch("default", internalFrames, internalFrames, getSampleRate());
 		aubio_pitch_set_silence(pitch, silence_threshold);
 	}
 
@@ -49,6 +50,14 @@ private:
 			param.ranges.max = getSampleRate() / 2;
 			param.ranges.def = 0;
 			break;
+		case 2:
+			param.hints = kParameterIsAutomable | kParameterIsOutput | kParameterIsBoolean;
+			param.name = "Tick";
+			param.symbol = "tick";
+			param.ranges.min = 0;
+			param.ranges.max = 1;
+			param.ranges.def = 0;
+			break;
 		default:
 			DISTRHO_SAFE_ASSERT(false);
 			break;
@@ -61,7 +70,10 @@ private:
 			return silence_threshold;
 			break;
 		case 1:
-			return pitch_estimate;
+			return (pitch_estimate == 0) ? -tick : pitch_estimate;
+			break;
+		case 2:
+			return tick;
 			break;
 		default:
 			DISTRHO_SAFE_ASSERT(false);
@@ -81,22 +93,28 @@ private:
 		// get the mono input and output
 		const float* const in  = inputs[0];
 		float* const out = outputs[0];
-		for (uint32_t i=0; i < frames; ++i) {
+		for (uint32_t i=0; i < internalFrames - frames; i++)
+			aubio_in->data[i] = aubio_in->data[i + frames];
+
+		for (uint32_t i=0; i < frames; i++) {
 			out[i] = in[i];
-			aubio_in->data[i] = in[i];
+			aubio_in->data[i + internalFrames - frames] = in[i];
 		}
 
 		aubio_pitch_do(pitch, aubio_in, aubio_out);
 		pitch_estimate = aubio_out->data[0];
-		std::cout << frames << " " << pitch_estimate << std::endl;
+		tick = !tick;
+//		std::cout << frames << " " << pitch_estimate << std::endl;
 	}
 
 private:
-	float silence_threshold;
+	float silence_threshold = -50;
 	fvec_t *aubio_in;
 	fvec_t *aubio_out;
 	aubio_pitch_t *pitch;
 	float pitch_estimate;
+	bool tick = false;
+	size_t internalFrames;
 };
 
 namespace DISTRHO {
--- plugins/outotune/OutotuneUI.cpp
+++ plugins/outotune/OutotuneUI.cpp
@@ -15,11 +15,17 @@ public:
 
 private:
     void parameterChanged(uint32_t index, float val) override {
-		if (index != 1)
+		switch (index) {
+		case 0:
 			return;
-		buffer.erase(buffer.begin());
-		buffer.push_back(val);
-		repaint();
+		case 1:
+			if (val < 0) val = 0;
+			buffer.erase(buffer.begin());
+			buffer.push_back(val);
+			break;
+		case 2:
+			break;
+		}
 	}
 
 	void onNanoDisplay() override {
@@ -28,25 +34,31 @@ private:
 
 		beginPath();
 		moveTo(0, 0);
-		bool prevZero = true;
+		float prev = 0;
 		for (size_t i = 0; i < buffer.size(); i++) {
 			auto f = normalize(buffer[i]);
 			auto x = (float)i / buffer.size() * w;
 			auto y = (1 - f) * h;
-			if (f > 0 && !prevZero)
+			if (f > 0 && prev > 0 && fabs(prev - f) < .02)
 				lineTo(x, y);
 			else
 				moveTo(x, y);
-			prevZero = f == 0;
+			prev = f;
 		}
 		strokeColor(255, 0, 0);
 		stroke();
 	}
 
 	float normalize(float freq) {
-		if (freq < 20)
+		static const float threshold_min = 50;
+		static const float threshold_max = 3000;
+		if (freq - threshold_min <= 20)
 			return 0;
-		return log(freq) / log(getSampleRate());
+		return (log(freq) - log(threshold_min)) / (log(threshold_max) - log(threshold_min));
+	}
+
+	void uiIdle() override {
+		repaint();
 	}
 
 private:
