2cb8e91d49011dec76c7d4f11f0b136af57a25a9
Richard Hladík <rihl@uralyx.cz>
2020-02-05 00:57:27 +0100

    Put pitch detection into separate files

--- plugins/outotune/OutotunePlugin.hpp
+++ plugins/outotune/OutotunePlugin.hpp
@@ -1,24 +1,22 @@
 #include "DistrhoPlugin.hpp"
 #include <atomic>
-#include <aubio/aubio.h>
 #include <cmath>
 #include <iostream>
+#include <aubio/aubio.h>
+
+#include "Pitch.hpp"
 
 class OutotunePlugin : public DISTRHO::Plugin {
 public:
 	OutotunePlugin() : Plugin(2, 0, 0) {
 		size_t frames = getBufferSize();
 		internalFrames = 4096;
-		aubio_in = new_fvec(internalFrames);
-		aubio_pitch = new_fvec(1);
-		aubio_out = new_fvec(frames);
+
 		auto rate = getSampleRate();
-
-		pitch = new_aubio_pitch("yinfast", internalFrames, frames, rate);
-		aubio_pitch_set_silence(pitch, silence_threshold);
-
+		aubio_out = new_fvec(frames);
 		wavetable = new_aubio_wavetable(rate, frames);
 		aubio_wavetable_play(wavetable);
+		estimator = createPitchEstimator(internalFrames, rate);
 	}
 
 private:
@@ -78,7 +76,7 @@ private:
 			break;
 		case 1:
 			hack->tick = !tick;
-			return (pitch_estimate == 0) ? -tick : pitch_estimate;
+			return (pitch == 0) ? -tick : pitch;
 			break;
 		case 2:
 			return tick;
@@ -94,28 +92,25 @@ private:
 		if (index)
 			return;
 		silence_threshold = val;
-		aubio_pitch_set_silence(pitch, silence_threshold);
+		DISTRHO_SAFE_ASSERT(false);
+		// TODO
+		//aubio_pitch_set_silence(pitch, silence_threshold);
 	}
 
 	void run(const float** inputs, float** outputs, uint32_t frames) override {
 		// get the mono input and output
 		const float* const in  = inputs[0];
 		float* const out = outputs[0];
-		for (uint32_t i=0; i < internalFrames - frames; i++)
-			aubio_in->data[i] = aubio_in->data[i + frames];
+		estimator->feed(in, frames);
 
-		for (uint32_t i=0; i < frames; i++)
-			aubio_in->data[i + internalFrames - frames] = in[i];
+		pitch = estimator->estimate();
+		confidence = estimator->getConfidence();
 
-		aubio_pitch_do(pitch, aubio_in, aubio_pitch);
-		confidence = aubio_pitch_get_confidence(pitch);
-		pitch_estimate = aubio_pitch->data[0];
-
-		if (pitch_estimate < 100 || pitch_estimate > 5000 || confidence < .6)
-			pitch_estimate = 0;
+		if (pitch < 100 || pitch > 5000 || confidence < .6)
+			pitch = 0;
 
 		aubio_wavetable_set_amp(wavetable, confidence * .5);
-		aubio_wavetable_set_freq(wavetable, pitch_estimate);
+		aubio_wavetable_set_freq(wavetable, pitch);
 		aubio_wavetable_do(wavetable, NULL, aubio_out);
 		for (uint32_t i=0; i < frames; i++)
 			out[i] = aubio_out->data[i];
@@ -124,12 +119,10 @@ private:
 
 private:
 	float silence_threshold = -50;
-	fvec_t *aubio_in;
-	fvec_t *aubio_pitch;
 	fvec_t *aubio_out;
-	aubio_pitch_t *pitch;
 	aubio_wavetable_t *wavetable;
-	float pitch_estimate = 0;
+	std::unique_ptr<PitchEstimator> estimator;
+	float pitch = 0;
 	float confidence = 0;
 	bool tick = false;
 	size_t internalFrames;
