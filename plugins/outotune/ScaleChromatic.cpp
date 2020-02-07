a63ef013eb58fded1533bd1cf6e6110c0068128a
Richard Hladík <rihl@uralyx.cz>
2020-02-07 01:17:42 +0100

    Reuse ScaleGeneric for ScaleChromatic
    The new version is probably a lot slower, but still O(1) and finding the
    closest scale tone is not going to be a bottleneck anyway.

--- plugins/outotune/ScaleChromatic.cpp
+++ plugins/outotune/ScaleChromatic.cpp
@@ -1,19 +1,5 @@
-#include <cmath>
-#include "Scale.hpp"
-
-class ChromaticScale : public Scale {
-public:
-	ChromaticScale() {}
-	float nearest_tone(float f) override {
-		auto semi = freq_to_semitones(f);
-		semi = roundf(semi);
-		f = semitones_to_freq(semi);
-		return f;
-	}
-	~ChromaticScale() override {}
-private:
-};
+#include "ScaleGeneric.hpp"
 
 std::unique_ptr<Scale> createScale() {
-	return std::make_unique<ChromaticScale>();
+	return std::make_unique<GenericScale>((1 << 12) - 1);
 }
